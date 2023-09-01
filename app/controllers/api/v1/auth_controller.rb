bbd75719bb04fb6e11cc22f3e3b8dc51d8c140e8
Mike <mikesil121@gmail.com>
2023-09-01 17:11:28 +0800

    Added skip_before_action for csrf issue sign_up

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::AuthController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token, only: %i(sign_up)
+
   def sign_up
     @user = User.create sign_up_params
 
