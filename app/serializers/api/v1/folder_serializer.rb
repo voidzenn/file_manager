4d6483e27ff10d131a532322f2c10ab485556f3e
Mike <mikesil121@gmail.com>
2024-04-01 14:19:57 +0800

    Removed slash path name

--- app/serializers/api/v1/folder_serializer.rb
+++ app/serializers/api/v1/folder_serializer.rb
@@ -1,3 +1,7 @@
 class Api::V1::FolderSerializer < ActiveModel::Serializer
-  attributes :id, :parent_folder_id, :path
+  attributes :id, :path, :parent_folder_id
+
+  attribute :path do
+    object&.path&.chop
+  end
 end
