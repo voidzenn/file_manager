a37d0ffab64482668b520e94156f1f230307429c
Michael Silverio <47482620+voidzenn@users.noreply.github.com>
2024-05-04 17:49:06 +0800

    [BE][Improvement][Folder-List-5] (#30)

--- app/serializers/api/v1/folder_serializer.rb
+++ app/serializers/api/v1/folder_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::FolderSerializer < ActiveModel::Serializer
-  attributes :id, :path, :parent_folder_id, :created_at
+  attributes :id, :unique_token, :path, :parent_folder_id, :created_at
 
   attribute :path do
     object&.path&.chop
