4ee8b98f4a41d7655db0476a89b01c1845fec75e
Mike <mikesil121@gmail.com>
2024-05-09 10:13:58 +0800

    Improved file upload serializer

--- app/serializers/api/v1/file_upload_serializer.rb
+++ app/serializers/api/v1/file_upload_serializer.rb
@@ -1,3 +1,11 @@
 class Api::V1::FileUploadSerializer < ActiveModel::Serializer
-  attributes :unique_token, :full_path, :name, :created_at
+  attributes :id, :unique_token, :full_path, :filename, :file_extension, :created_at
+
+  attribute :filename do
+    object.name&.split('.')&.first
+  end
+
+  attribute :file_extension do
+    object.name&.split('.')&.last
+  end
 end
