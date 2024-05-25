dd9f8b9bdc8e514b7fd1fe13dbc49c8d1ea5ef92
Michael Silverio <47482620+voidzenn@users.noreply.github.com>
2024-05-25 21:48:24 +0800

    [BE][Improvement][Dockerized-Server] (#33)

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Load environment variables from .env file in development and test environments
+Dotenv::Rails.load if %w[development test].include? ENV['RAILS_ENV']
+
 module FileManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,9 +4,10 @@ require "aws-sdk-s3"
 
 Aws.config.update(
   credentials: Aws::Credentials.new(
-    ENV["AWS_ACCESS_KEY_ID"],
-    ENV["AWS_SECRET_ACCESS_KEY"]
+    ENV["MINIO_ACCESS_KEY_ID"],
+    ENV["MINIO_SECRET_ACCESS_KEY"]
   ),
-  region: ENV["AWS_REGION"],
-  endpoint: ENV["AWS_ENDPOINT"]
+  region: ENV["MINIO_REGION"],
+  endpoint: ENV["MINIO_ENDPOINT"],
+  force_path_style: true
 )
