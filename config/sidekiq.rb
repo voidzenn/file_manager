d6204e15acf09417cd65e94e880aa2de332d95ac
Mike <mikesil121@gmail.com>
2024-05-25 21:45:37 +0800

    Improved docker configurations

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' } # Replace with your Redis server information
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' } # Replace with your Redis server information
+  config.redis = { url: ENV['REDIS_URL'] }
 end
