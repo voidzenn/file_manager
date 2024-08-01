45c88e1853c385434568b50a058d8a4ebf3d3d1c
Mike <mikesil121@gmail.com>
2024-08-01 18:02:18 +0800

    Minor improvement on signup specs and added sigin in specs

--- spec/requests/api/v1/auth_spec.rb
+++ spec/requests/api/v1/auth_spec.rb
@@ -3,14 +3,14 @@
 require "swagger_helper"
 
 RSpec.describe "Auth API", type: :request do
-  shared_context "missing field errors" do |field_name|
+  shared_context :missing_field_errors do |field_name|
     let(:valid_params) do
       {
         email: "email@email.com",
         password: "Password12!",
         fname: "John",
         lname: "Doe"
-      }.except(field_name.to_sym)
+      }.except(field_name)
     end
     let(:user) { { user: valid_params } }
 
@@ -43,12 +43,18 @@ RSpec.describe "Auth API", type: :request do
       parameter name: :user, in: :body, schema: {
         type: :object,
         properties: {
-          email: { type: :string },
-          password: { type: :string },
-          fname: { type: :string },
-          lname: { type: :string }
+          user: {
+            type: :object,
+            properties: {
+              email: { type: :string },
+              password: { type: :string },
+              fname: { type: :string },
+              lname: { type: :string }
+            }
+          },
+          required: [ :email, :password, :fname, :lname ]
         },
-        required: [ 'email', 'password', 'fname', 'lname' ]
+        require: [ :user ]
       }
 
       response "201", "created" do
@@ -73,29 +79,82 @@ RSpec.describe "Auth API", type: :request do
         end
       end
 
-      response "422", "parameter missing" do
-        examples "application/json" => {
-          success: false,
-          error: [{"<attribute_name>":"cannot be blank"}]
-        }
+      response "422", "unprocessable_entity" do
+        context "when parameter missing" do
+          example "application/json", :parameter_missing, {
+            success: false,
+            error: [{"<attribute_name>":"cannot be blank"}]
+          }
 
-        it_behaves_like "missing field errors", "email"
-        it_behaves_like "missing field errors", "password"
-        it_behaves_like "missing field errors", "fname"
-        it_behaves_like "missing field errors", "lname"
+          it_behaves_like :missing_field_errors, :email
+          it_behaves_like :missing_field_errors, :password
+          it_behaves_like :missing_field_errors, :fname
+          it_behaves_like :missing_field_errors, :lname
+        end
+
+        context "when email is invalid" do
+          example "application/json", :email_invalid, {
+            success: false,
+            error: [{email: "is invalid"}]
+          }
+
+          let(:params) do
+            valid_params[:email] = "user.com"
+            valid_params
+          end
+          let(:user) { { user: params } }
+
+          run_test! "returns email is invalid error message" do
+            expect(response_body[:error][0][:email]).to eq "is invalid"
+          end
+        end
+
+        context "when email already exists" do
+          example "application/json", :email_exists, {
+            success: false,
+            error: "Parameter missing"
+          }
+
+          let!(:new_user) { create(:user, email: valid_params[:email]) }
+          let(:user) { { user: valid_params } }
+
+          run_test! "returns emails exists error message" do
+            expect(response_body[:error][0][:email]).to eq "already exists"
+          end
+        end
       end
+    end
+  end
 
-      response "422", "email exists" do
-        let!(:new_user) { create(:user, email: valid_params[:email]) }
-        let(:user) { { user: valid_params } }
+  path "/api/v1/auth/sign_in" do
+    post "Sign In" do
+      tags "Auth"
+      consumes "application/json"
+      produces "application/json"
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string }
+        },
+        required: [ :email, :password ]
+      }
 
-        examples "application/json" => {
-          success: false,
-          error: "Parameter missing"
-        }
+      response "200", "ok" do
+        let!(:new_user) { create :user }
+        let(:user) do
+          {
+            email: new_user.email,
+            password: new_user.password
+          }
+        end
 
         run_test! do
-          expect(response_body[:error][0][:email]).to eq "already exists"
+          expect(response_body[:success]).to eq true
+          expect(response_body[:meta][:token]).to eq assigns(:token)
+          expect(response_body[:data][:email]).to eq new_user.email
+          expect(response_body[:data][:fname]).to eq new_user.fname
+          expect(response_body[:data][:lname]).to eq new_user.lname
         end
       end
     end
