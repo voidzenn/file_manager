6238251f8cd1ce9b0bdd190d58e91c60ee2c429e
Mike <mikesil121@gmail.com>
2024-08-07 01:57:06 +0800

    Improved seed and updated constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 # config
 REQUEST_TIMEOUT = 30 # seconds
 
-MINIO_GET_FILE_EXPIRATION =  3000 # 5 minutes
+MINIO_GET_FILE_EXPIRATION =  300 # 5 minutes
 
 FOLDER_CHANNEL = "folder_channel"
 FOLDER_CREATED = "folder_created"
